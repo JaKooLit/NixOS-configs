bbab602be60e6b4156aec1a9c7a85bfa1be6a494
Ja.KooLit <ejhay.games@gmail.com>
2024-06-09 02:18:56 +0900

    MiniPC

--- HP-Mini/flake.nix
+++ HP-Mini/flake.nix
@@ -1,15 +1,15 @@
 {
-  	description = "HP-MiniPC"; 
+  description = "HP-MiniPC"; 
   	
-		inputs = {
-			nixpkgs.url = "nixpkgs/nixos-unstable";
-			wallust.url = "git+https://codeberg.org/explosion-mental/wallust?ref=dev";
-			#hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1"; # unstable hyprland
-			distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes"; 
+  inputs = {
+  	nixpkgs.url = "nixpkgs/nixos-unstable";
+	wallust.url = "git+https://codeberg.org/explosion-mental/wallust?ref=dev";
+	#hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1"; # unstable hyprland
+	distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes"; 
   	};
 
-  	outputs = 
-			inputs@{ self,nixpkgs, ... }:
+  outputs = 
+	inputs@{ self,nixpkgs, ... }:
     	let
       	system = "x86_64-linux";
       	host = "NixOS-MiniPC";
@@ -21,18 +21,18 @@
           	allowUnfree = true;
         	};
       	};
-    	in
-    	{
-			nixosConfigurations = {
-        "${host}" = nixpkgs.lib.nixosSystem rec {
-				specialArgs = { 
-					inherit system;
-					inherit inputs;
-          inherit username;
-          inherit host;
-					};
-	    		modules = [ ./configuration.nix ];
-	  			};
-				};
+    in
+    {
+	nixosConfigurations = {
+      "${host}" = nixpkgs.lib.nixosSystem rec {
+		specialArgs = { 
+			inherit system;
+			inherit inputs;
+    	    inherit username;
+       		inherit host;
 			};
+	   		modules = [ ./configuration.nix ];
+			};
+		};
+	};
 }
