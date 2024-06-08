b597c012be273b4a5f1faf0209bf9669ac9b3bb9
Ja.KooLit <ejhay.games@gmail.com>
2024-06-09 00:52:08 +0900

    updated miniPC

--- HP-Mini/profiles/Mini-PC/HP-Mini.nix
+++ HP-Mini/profiles/Mini-PC/HP-Mini.nix
@@ -5,37 +5,41 @@
 
   {
   # Kernel Parameters miniPC
-  #boot.loader.grub.theme = "/boot/grub/themes/nixos/";
-  boot.kernelParams = [ 
+  boot = {
+    kernelParams = [ 
     "nowatchdog"
-	"modprobe.blacklist=iTCO_wdt"
- 	];
+	  "modprobe.blacklist=iTCO_wdt"
+ 	  ];
   
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+    initrd = { 
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+      kernelModules = [ ];
+    };
+  
+    kernelModules = [ ];
+    extraModulePackages = [ ];
 
-  # bootloader grub theme
-  boot.loader.grub = rec {
-    theme = inputs.distro-grub-themes.packages.${system}.nixos-grub-theme;
-    splashImage = "${theme}/splash_image.jpg";
+    # bootloader grub theme
+    loader.grub = rec {
+      theme = inputs.distro-grub-themes.packages.${system}.nixos-grub-theme;
+      splashImage = "${theme}/splash_image.jpg";
+    };
+
+    # Kernel 
+    kernelPackages = pkgs.linuxPackages_latest;
   };
-
-  # Kernel 
-  #boot.kernelPackages = pkgs.linuxPackages_latest;
-
+  
   networking.hostName = "NixOS-MiniPC";
   
   # User account
   users = {
-	users.ja = {
-      isNormalUser = true;
-      extraGroups = [ 
-		"wheel" 
-		"video" 
-		"input" 
-		"audio"
+	  users.ja = {
+    isNormalUser = true;
+    extraGroups = [ 
+		  "wheel" 
+		  "video" 
+		  "input" 
+		  "audio"
 		]; 
     packages = with pkgs; [		
      	];
@@ -48,27 +52,28 @@
 
   # for HP - Mini pc
   environment.systemPackages = with pkgs; [
-	discord
-	fzf
+	  discord
+	  fzf
     glxinfo
-	krabby
+	  krabby
     vscodium
-	nvtopPackages.intel # requires unstable channel
+	  nvtopPackages.intel # requires unstable channel
   ];
+
   # Additional fonts needed for office stuff
   fonts.packages = with pkgs; [
-	cascadia-code
- 	];
+	  cascadia-code
+ 	  ];
 	
   powerManagement = {
-	enable = true;
-	cpuFreqGovernor = "performance";
+	  enable = true;
+	  cpuFreqGovernor = "performance";
   };
     
   # Zsh configuration
   programs.zsh = {
     enable = true;
-	enableCompletion = true;
+	  enableCompletion = true;
       ohMyZsh = {
         enable = true;
         plugins = ["git"];
@@ -77,13 +82,13 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     promptInit = ''
-	krabby random --no-mega --no-gmax --no-regional --no-title -s;
-      	source <(fzf --zsh);
-	HISTFILE=~/.zsh_history;
-	HISTSIZE=10000;
-	SAVEHIST=10000;
-	setopt appendhistory;
-    '';
+	    krabby random --no-mega --no-gmax --no-regional --no-title -s;
+      source <(fzf --zsh);
+	    HISTFILE=~/.zsh_history;
+	    HISTSIZE=10000;
+	    SAVEHIST=10000;
+	    setopt appendhistory;
+      '';
   };
   
   # Enable CUPS to print documents.
@@ -91,16 +96,16 @@
     
   # HARDWARES:
   hardware = {
-	bluetooth = {
-		enable = true;
-		powerOnBoot = true;
-		settings = {
-			General = {
-			Enable = "Source,Sink,Media,Socket";
-			Experimental = true;
-			};
-		};
-	};
+    bluetooth = {
+	    enable = true;
+	    powerOnBoot = true;
+	    settings = {
+		  General = {
+		    Enable = "Source,Sink,Media,Socket";
+		    Experimental = true;
+			  };
+		  };
+	  };
 
 	cpu.intel.updateMicrocode = true;
 
@@ -108,24 +113,20 @@
     	enable = true;
     	driSupport = true;
     	driSupport32Bit = true;
-		extraPackages = with pkgs; [
+		  extraPackages = with pkgs; [
    			libva
-			libva-utils	
+			  libva-utils	
      		];
   	};
 
   }; 
 
   services = {
-	watchdogd.enable = false; # watchdog
-	blueman.enable = true;
-
-	xserver.videoDrivers = ["intel"];
-
-	#flatpak.enable = true;
+	  blueman.enable = true;
+	  xserver.videoDrivers = ["intel"];
+	  #flatpak.enable = true;
   };
   
-	
   # For Electron apps to use wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   
