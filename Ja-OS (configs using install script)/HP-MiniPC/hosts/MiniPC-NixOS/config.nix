2800e8daaf561a1e796d9b4dabc9c6e28c4afa5e
JaKooLit <ejhay.games@gmail.com>
2024-12-29 06:50:59 +0900

    updated NixOS Config for mini-PC

--- Ja-OS (configs using install script)/HP-MiniPC/hosts/MiniPC-NixOS/config.nix
+++ Ja-OS (configs using install script)/HP-MiniPC/hosts/MiniPC-NixOS/config.nix
@@ -3,18 +3,12 @@
 { config, pkgs, host, username, options, lib, inputs, system, ...}: let
   
   inherit (import ./variables.nix) keyboardLayout;
-  python-packages = pkgs.python3.withPackages (
-    ps:
-      with ps; [
-        requests
-        pyquery # needed for hyprland-dots Weather script
-        ]
-    );
   
   in {
   imports = [
     ./hardware.nix
     ./users.nix
+	./packages-fonts.nix
     ../../modules/amd-drivers.nix
     ../../modules/nvidia-drivers.nix
     ../../modules/nvidia-prime-drivers.nix
@@ -181,95 +175,6 @@
   users = {
     mutableUsers = true;
   };
-
-  environment.systemPackages = (with pkgs; [
-  # System Packages
-    baobab
-    btrfs-progs
-    clang
-    curl
-    cpufrequtils
-    duf
-    eza
-    ffmpeg   
-    glib #for gsettings to work
-    gsettings-qt
-    git
-    killall  
-    libappindicator
-    libnotify
-    openssl #required by Rainbow borders
-    pciutils
-    vim
-    wget
-    xdg-user-dirs
-    xdg-utils
-
-    fastfetch
-    (mpv.override {scripts = [mpvScripts.mpris];}) # with tray
-      
-    # Hyprland Stuff
-    ags        
-    btop
-    cava
-    cliphist
-    eog
-    gnome-system-monitor
-    file-roller
-    grim
-    gtk-engine-murrine #for gtk themes
-    hyprcursor # requires unstable channel
-    hypridle # requires unstable channel
-    imagemagick 
-    inxi
-    jq
-    kitty
-    libsForQt5.qtstyleplugin-kvantum #kvantum
-    networkmanagerapplet
-    nwg-look # requires unstable channel
-    nvtopPackages.intel # requires unstable channel
-    pamixer
-    pavucontrol
-    playerctl
-    polkit_gnome
-    pyprland
-    qt5ct
-    qt6ct
-    qt6.qtwayland
-    qt6Packages.qtstyleplugin-kvantum #kvantum
-    rofi-wayland
-    slurp
-    swappy
-    swaynotificationcenter
-    swww
-    unzip
-    wallust
-    wl-clipboard
-    wlogout
-    yad
-    yt-dlp
-
-	#added to miniPC
-	discord
-	ranger
-	vscodium
-
-    #waybar  # if wanted experimental next line
-    #(pkgs.waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];}))
-  ]) ++ [
-	python-packages
-  ];
-
-  # FONTS
-  fonts.packages = with pkgs; [
-    noto-fonts
-    fira-code
-    noto-fonts-cjk
-    jetbrains-mono
-    font-awesome
-	terminus_font
-    (nerdfonts.override {fonts = ["JetBrainsMono"];})
- 	];
 
   # Extra Portal Configuration
   xdg.portal = {
